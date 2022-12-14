b86e7e5bedbd11db5a534958b52aca99d04b647e
Anirudh A V <anirudh.av02@gmail.com>
2022-12-15 01:29:49 +0530

    refacter: minor changes

--- index.js
+++ index.js
@@ -17,10 +17,6 @@ app.use(
   })
 )
 
-// app.get('/', (request, response) => {
-//   response.json({ info: 'Node.js, Express, and Postgres API' })
-// })
-
 app.get('/users', db.getUsers)
 app.get('/users/:id', db.getUserById)
 app.post('/users', db.createUser)
