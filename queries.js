33136958a8565cb99df3a83a4b36f2efaa3065a0
Anirudh A V <anirudh.av02@gmail.com>
2022-12-28 09:54:46 +0530

    refactor: location field added to complaint table

--- queries.js
+++ queries.js
@@ -319,9 +319,9 @@ const getComplaintById = (request, response) => {
 }
 
 const createComplaint = (request, response) => {
-    const { consumer_id, description, date } = request.body
+    const { consumer_id, description, date, location } = request.body
 
-    pool.query('INSERT INTO complaint (consumer_id, description, date) VALUES ($1, $2, $3)', [consumer_id, description, date], (error, results) => {
+    pool.query('INSERT INTO complaint (consumer_id, description, date, location) VALUES ($1, $2, $3, $4)', [consumer_id, description, date, location], (error, results) => {
         if (error) {
             return response.status(400).json({
                 success: false,
@@ -329,17 +329,17 @@ const createComplaint = (request, response) => {
                 message: error.message
             })
         }
-        response.status(201).send(`Complaint added with ID: ${results.insertId}`)
+        response.status(201).send(`Complaint added with ID: ${results.oid}`)
     })
 }
 
 const updateComplaint = (request, response) => {
     const id = parseInt(request.params.id)
-    const { description, date } = request.body
+    const { description, date, location } = request.body
 
     pool.query(
-        'UPDATE complaint SET description = $2, date = $3 WHERE complaint_id = $1',
-        [id, description, date],
+        'UPDATE complaint SET description = $2, date = $3, location = $4 WHERE complaint_id = $1',
+        [id, description, date, location],
         (error, results) => {
             if (error) {
                 return response.status(400).json({
