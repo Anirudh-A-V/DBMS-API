1d861294ad83b75170777fe1ca03dac4633f6300
Anirudh A V <anirudh.av02@gmail.com>
2022-12-29 09:53:10 +0530

    fix: complaint resolve endpoint fixed

--- index.js
+++ index.js
@@ -155,7 +155,7 @@ app.post('/complaint', db.createComplaint)
 app.put('/complaint/:id', db.updateComplaint)
 app.delete('/complaint/:id', db.deleteComplaint)
 app.get('/complaint/consumer/:id', db.getComplaintByConsumerId)
-app.put('/complaint/resolved/:id', db.resolveComplaint)
+app.put('/complaint/resolve/:id', db.resolveComplaint)
 
 app.get('/admin', db.getAdmins)
 app.get('/admin/:id', db.getAdminById)
